b30d47c7d1e59b8247e58675976110d28febd369
Linhi01 <dominik.linhart@infis.cz>
2025-05-27 11:28:46 +0200

    Add files via upload

---
+++ funkce s paramentry.py
@@ -0,0 +1,20 @@
+#1
+def cislo1(cislo):
+    nasobek = cislo * 5
+
+    print(nasobek)
+
+cislo1(5)
+
+#2
+def pole1(pole):
+    print(pole[0])
+
+pole1("pes,kočka")
+
+#3
+pocet = (input)
+
+
+def pocet():
+    
