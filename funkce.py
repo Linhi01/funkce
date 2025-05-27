d9f98574fc379611d3a185ad12cc8cc00237db1e
Linhi01 <dominik.linhart@infis.cz>
2025-05-27 11:28:35 +0200

    Add files via upload

---
+++ funkce.py
@@ -0,0 +1,42 @@
+print("zadej číslo podle, kterého zapnu funkci")
+volba = input()
+if volba == (1)
+   
+ 
+ 
+ 
+ 
+ 
+ 
+def pozdrav():
+    print("ahoj, jak se máš?")
+ 
+pozdrav()
+ 
+def dnesni_den():
+    print("dnes je úterý")
+ 
+dnesni_den()
+ 
+def cislo():
+    print("1")
+    print("2")
+    print("3")
+    print("4")
+    print("5")
+ 
+cislo()
+ 
+def hvezdy():
+    print("* * * * *")
+    print("* * * * *")
+    print("* * * * *")
+ 
+hvezdy()
+ 
+def nalada_dne():
+    import random
+    nalada = ["dobrá", "špatná"]
+    nahodna_hodnota = random.choice(nalada)
+ 
+    print("nálada je:", )
